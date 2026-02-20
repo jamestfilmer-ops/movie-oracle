78e5d350e2c4838a6bc120014421dd49d1da1e18
jamestfilmer-ops <jamestfilmer@gmail.com>
2026-02-19 21:38:27 -0600

    Update movies.js

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,1 +1,6 @@
-
+export const watchlist = [
+  { title: "The Lion King", year: 1994, tmdb_id: 8587 },
+  { title: "Spirited Away", year: 2001, tmdb_id: 129 },
+  { title: "Wall-E", year: 2008, tmdb_id: 10681 },
+  // Add more here! Just the title and year is usually enough for the API to find it.
+];
